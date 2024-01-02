d18634170cdb06d5125402682a28d4ce97e113e3
BoiseITGuru <dev@boiseitguru.com>
2024-01-02 08:35:35 -0700

    ios project

---
+++ iOS/FAH/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FAH
+//
+//  Created by BoiseITGuru on 1/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/FAH/FAHApp.swift
@@ -0,0 +1,17 @@
+//
+//  FAHApp.swift
+//  FAH
+//
+//  Created by BoiseITGuru on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FAHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
